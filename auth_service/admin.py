bd9503aed551c73858703144600cc4d690b58243
ali <ali.darzi.1354@gmail.com>
2024-04-07 04:30:37 +0330

    UserLogin -> admin !

--- auth_service/admin.py
+++ auth_service/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+admin.site.register(models.UserProfile)
+admin.site.register(models.UserLogins)
+admin.site.register(models.UserDevice)
+admin.site.register(models.UserIP)
